8862fce33a02a7d06cc39a181146a57798d99abd
BiancaPalileo <biclpalileo@gmail.com>
2022-01-27 10:29:02 +0000

    ESNOTE-58 #comment Adding state to Render form for Registration

--- client/src/components/NonUserHome/NonUserHome.js
+++ client/src/components/NonUserHome/NonUserHome.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 import Images from "../../images/Images";
 
 import { Grid } from "./Grid.style";
 import { Header } from "./Header.style";
 import { Content } from "./Content.style";
 import { Form } from "./Form.style";
+import Registration from "./Registration";
 
 const NonUserHome = () => {
+
+    const [showRegistration, setShowRegistration] = useState(false);
+
+    const openSignUpForm = () => {
+        setShowRegistration(prev => !prev);
+        console.log(showRegistration);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <Grid>
             <Header>
@@ -15,19 +30,21 @@ const NonUserHome = () => {
                 </div>
             </Header>
 
+            {   showRegistration && <Registration /> }
+
             <Content>
                 <div id="presentation">
                 </div>
 
                 <div id="register-login">
-                    <Form>
+                    <Form onSubmit={ (e) => handleSubmit(e)}>
                         <h1>Login</h1>
                         <input placeholder="Email or Username"></input>
                         <input placeholder="Password" type="password"></input>
                         <button id="login">Login</button>
                         <hr></hr>
                         <h1>Register</h1>
-                        <button id="registration">Sign Up</button>
+                        <button id="registration" onClick={ openSignUpForm }>Sign Up</button>
                     </Form>
                 </div>
             </Content>
